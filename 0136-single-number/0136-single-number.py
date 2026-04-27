08aa43737bdeb4c6572b1dc0522288a0b86c4a1a
sai-pix <kurangisai92@gmail.com>
2026-04-27 16:18:40 +0530

    Time: 3 ms (61.72%), Space: 21.1 MB (46.9%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d={}
+        res=0
         for i in nums:
-            d[i]=d.get(i,0)+1
-        for i,j in d.items():
-            if j==1:
-                return i
+            res^=i
+        return res
