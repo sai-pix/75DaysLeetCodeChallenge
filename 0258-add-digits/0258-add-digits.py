9d426d23bfda3fbb7220b888f3f4c449940c015a
sai-pix <kurangisai92@gmail.com>
2026-06-06 15:25:42 +0530

    Time: 0 ms (100%), Space: 19.3 MB (16.74%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num<10:
+            return num
+        s=0
+        while num:
+            s+=num%10
+            num//=10
+        return self.addDigits(s)
